1d6ba5a62051fbcee8c3f5331641ec2045448f33
septem9907 <septem9907@gmail.com>
2019-04-14 18:03:16 +0800

    finish setup in Ubuntu

--- code/A_Gentle_Introduction_to_Spark-Chapter_2_A_Gentle_Introduction_to_Spark.py
+++ code/A_Gentle_Introduction_to_Spark-Chapter_2_A_Gentle_Introduction_to_Spark.py
@@ -12,7 +12,7 @@ flightData2015 = spark\
   .read\
   .option("inferSchema", "true")\
   .option("header", "true")\
-  .csv("/data/flight-data/csv/2015-summary.csv")
+  .csv("./data/flight-data/csv/2015-summary.csv")
 
 # COMMAND ----------
 
