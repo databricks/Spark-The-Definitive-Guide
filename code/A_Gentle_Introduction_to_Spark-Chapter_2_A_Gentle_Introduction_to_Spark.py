88f7d9f0d860fe1a43cb17618bedb4cc1a45ea51
septem9907 <septem9907@gmail.com>
2020-05-10 16:11:12 +0800

    categorize files in different languages

--- code/A_Gentle_Introduction_to_Spark-Chapter_2_A_Gentle_Introduction_to_Spark.py
+++ code/A_Gentle_Introduction_to_Spark-Chapter_2_A_Gentle_Introduction_to_Spark.py
@@ -1,24 +1,27 @@
-#%%
+# %%
 from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName("spark-book").getOrCreate()
 
-#%%
+# %%
+spark
+
+# %%
 myRange = spark.range(1000).toDF("number")
 
-#%%
+# %%
 divisBy2 = myRange.where("number % 2 = 0")
 
-#%%
-flightData2015 = spark\
-  .read\
-  .option("inferSchema", "true")\
-  .option("header", "true")\
-  .csv("./data/flight-data/csv/2015-summary.csv")
-
-#%%
+# %%
+flightData2015 = (
+    spark.read
+    .option("inferSchema", "true")
+    .option("header", "true")
+    .csv("../data/flight-data/csv/2015-summary.csv")
+)
+# %%
 flightData2015.createOrReplaceTempView("flight_data_2015")
 
-#%%
+# %%
 sqlWay = spark.sql("""
 SELECT DEST_COUNTRY_NAME, count(1)
 FROM flight_data_2015
@@ -32,12 +35,12 @@ dataFrameWay = flightData2015\
 sqlWay.explain()
 dataFrameWay.explain()
 
-#%%
+# %%
 from pyspark.sql.functions import max
 
 flightData2015.select(max("count")).take(1)
 
-#%%
+# %%
 maxSql = spark.sql("""
 SELECT DEST_COUNTRY_NAME, sum(count) as destination_total
 FROM flight_data_2015
@@ -48,22 +51,24 @@ LIMIT 5
 
 maxSql.show()
 
-#%%
+# %%
 from pyspark.sql.functions import desc
 
-flightData2015\
-  .groupBy("DEST_COUNTRY_NAME")\
-  .sum("count")\
-  .withColumnRenamed("sum(count)", "destination_total")\
-  .sort(desc("destination_total"))\
-  .limit(5)\
-  .show()
-
-#%%
-flightData2015\
-  .groupBy("DEST_COUNTRY_NAME")\
-  .sum("count")\
-  .withColumnRenamed("sum(count)", "destination_total")\
-  .sort(desc("destination_total"))\
-  .limit(5)\
-  .explain()
+(flightData2015
+    .groupBy("DEST_COUNTRY_NAME")
+    .sum("count")
+    .withColumnRenamed("sum(count)", "destination_total")
+    .sort(desc("destination_total"))
+    .limit(5)
+    .show()
+)
+# %%
+(flightData2015
+    .groupBy("DEST_COUNTRY_NAME")
+    .sum("count")
+    .withColumnRenamed("sum(count)", "destination_total")
+    .sort(desc("destination_total"))
+    .limit(5)
+    .explain()
+)
+# %%
--- code/A_Gentle_Introduction_to_Spark-Chapter_3_A_Tour_of_Sparks_Toolset.py
+++ code/A_Gentle_Introduction_to_Spark-Chapter_3_A_Tour_of_Sparks_Toolset.py
@@ -1,59 +1,58 @@
-staticDataFrame = spark.read.format("csv")\
-  .option("header", "true")\
-  .option("inferSchema", "true")\
-  .load("/data/retail-data/by-day/*.csv")
+# %%
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.appName("spark-book").getOrCreate()
+
+# %%
+spark
+
+# %%
+staticDataFrame = (
+    spark.read.format("csv")
+    .option("header", "true")
+    .option("inferSchema", "true")
+    .load("../data/retail-data/by-day/*.csv")
+)
 
 staticDataFrame.createOrReplaceTempView("retail_data")
 staticSchema = staticDataFrame.schema
 
-
-# COMMAND ----------
-
+# %%
 from pyspark.sql.functions import window, column, desc, col
-staticDataFrame\
-  .selectExpr(
-    "CustomerId",
-    "(UnitPrice * Quantity) as total_cost",
-    "InvoiceDate")\
-  .groupBy(
-    col("CustomerId"), window(col("InvoiceDate"), "1 day"))\
-  .sum("total_cost")\
-  .show(5)
 
+(staticDataFrame
+    .selectExpr(
+    "CustomerId", "(UnitPrice * Quantity) as total_cost", "InvoiceDate")
+    .groupBy(col("CustomerId"), window(col("InvoiceDate"), "1 day"))
+    .sum("total_cost")
+    .show(5)
+)
 
-# COMMAND ----------
-
-streamingDataFrame = spark.readStream\
-    .schema(staticSchema)\
-    .option("maxFilesPerTrigger", 1)\
-    .format("csv")\
+# %%
+streamingDataFrame = (
+    spark.readStream
+    .schema(staticSchema)
+    .option("maxFilesPerTrigger", 1)
+    .format("csv")
     .option("header", "true")\
     .load("/data/retail-data/by-day/*.csv")
+)
 
+# %%
+purchaseByCustomerPerHour = (
+    streamingDataFrame
+    .selectExpr("CustomerId", "(UnitPrice * Quantity) as total_cost", "InvoiceDate")
+    .groupBy(col("CustomerId"), window(col("InvoiceDate"), "1 day"))
+    .sum("total_cost")
+)
 
-# COMMAND ----------
-
-purchaseByCustomerPerHour = streamingDataFrame\
-  .selectExpr(
-    "CustomerId",
-    "(UnitPrice * Quantity) as total_cost",
-    "InvoiceDate")\
-  .groupBy(
-    col("CustomerId"), window(col("InvoiceDate"), "1 day"))\
-  .sum("total_cost")
-
-
-# COMMAND ----------
-
+# %%
 purchaseByCustomerPerHour.writeStream\
     .format("memory")\
     .queryName("customer_purchases")\
     .outputMode("complete")\
     .start()
 
-
-# COMMAND ----------
-
+# %%
 spark.sql("""
   SELECT *
   FROM customer_purchases
@@ -61,91 +60,81 @@ spark.sql("""
   """)\
   .show(5)
 
-
-# COMMAND ----------
-
+# %%
 from pyspark.sql.functions import date_format, col
-preppedDataFrame = staticDataFrame\
-  .na.fill(0)\
-  .withColumn("day_of_week", date_format(col("InvoiceDate"), "EEEE"))\
-  .coalesce(5)
+preppedDataFrame = (
+    staticDataFrame
+    .na.fill(0)
+    .withColumn("day_of_week", date_format(col("InvoiceDate"), "EEEE"))
+    .coalesce(5)
+)
 
+# %%
+trainDataFrame = (
+    preppedDataFrame
+    .where("InvoiceDate < '2011-07-01'")
+)
 
-# COMMAND ----------
+testDataFrame = (
+    preppedDataFrame
+    .where("InvoiceDate >= '2011-07-01'")
+)
 
-trainDataFrame = preppedDataFrame\
-  .where("InvoiceDate < '2011-07-01'")
-testDataFrame = preppedDataFrame\
-  .where("InvoiceDate >= '2011-07-01'")
-
-
-# COMMAND ----------
-
+# %%
 from pyspark.ml.feature import StringIndexer
-indexer = StringIndexer()\
-  .setInputCol("day_of_week")\
-  .setOutputCol("day_of_week_index")
+indexer = (
+    StringIndexer()
+    .setInputCol("day_of_week")
+    .setOutputCol("day_of_week_index")
+)
 
-
-# COMMAND ----------
+# %%
 
 from pyspark.ml.feature import OneHotEncoder
-encoder = OneHotEncoder()\
-  .setInputCol("day_of_week_index")\
-  .setOutputCol("day_of_week_encoded")
+encoder = (
+    OneHotEncoder()
+    .setInputCol("day_of_week_index")
+    .setOutputCol("day_of_week_encoded")
+)
 
-
-# COMMAND ----------
-
+# %%
 from pyspark.ml.feature import VectorAssembler
 
-vectorAssembler = VectorAssembler()\
-  .setInputCols(["UnitPrice", "Quantity", "day_of_week_encoded"])\
-  .setOutputCol("features")
+vectorAssembler = (
+    VectorAssembler()
+    .setInputCols(["UnitPrice", "Quantity", "day_of_week_encoded"])
+    .setOutputCol("features")
+)
 
-
-# COMMAND ----------
-
+# %%
 from pyspark.ml import Pipeline
 
-transformationPipeline = Pipeline()\
-  .setStages([indexer, encoder, vectorAssembler])
+transformationPipeline = (
+    Pipeline()
+    .setStages([indexer, encoder, vectorAssembler])
+)
 
-
-# COMMAND ----------
-
+# %%
 fittedPipeline = transformationPipeline.fit(trainDataFrame)
 
-
-# COMMAND ----------
-
+# %%
 transformedTraining = fittedPipeline.transform(trainDataFrame)
 
-
-# COMMAND ----------
-
+# %%
 from pyspark.ml.clustering import KMeans
 kmeans = KMeans()\
   .setK(20)\
-  .setSeed(1L)
+  .setSeed(1)
 
-
-# COMMAND ----------
-
+# %%
 kmModel = kmeans.fit(transformedTraining)
 
-
-# COMMAND ----------
-
+# %%
 transformedTest = fittedPipeline.transform(testDataFrame)
 
-
-# COMMAND ----------
-
+# %%
 from pyspark.sql import Row
 
 spark.sparkContext.parallelize([Row(1), Row(2), Row(3)]).toDF()
 
-
-# COMMAND ----------
-
+# %%
